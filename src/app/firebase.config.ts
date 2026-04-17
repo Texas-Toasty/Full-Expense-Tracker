0aa4ebb89bb00a0846e661ad2e6a82546be22d43
Texas-Toasty <reecedparry@gmail.com>
2026-04-17 17:05:26 -0500

    Attempted fix for firebase api leak

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyC_wmXcQ-vxBjbhyYWorqw-0a8zeKj60p4',
-  authDomain: 'full-expense-tracker-8ce6e.firebaseapp.com',
-  projectId: 'full-expense-tracker-8ce6e',
-  storageBucket: 'full-expense-tracker-8ce6e.firebasestorage.app',
-  messagingSenderId: '953746821641',
-  appId: '1:953746821641:web:8cabcef6c659bccca19ed8',
-};
